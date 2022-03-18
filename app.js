cba7cbbd0b0bc385f4287c62867e0e5d594285a7
Ruben Figueiredo <ruben.barroso.figueiredo@gmail.com>
2022-03-18 12:32:18 +0000

    declarar as primeiras variaveis

---
+++ app.js
@@ -0,0 +1,4 @@
+const player = document.getElementById('player_choice');
+const computer = document.getElementById('compuer_choice');
+const playerScore = document.getElementById('player_score');
+const computerScore = document.getElementById('computser_score');
