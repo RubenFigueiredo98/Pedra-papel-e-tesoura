87433e5881395b4b26103d1f088008f62aae55bd
Ruben Figueiredo <ruben.barroso.figueiredo@gmail.com>
2022-03-18 13:54:15 +0000

    Adding computer choice and ending logic of the game

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const player = document.getElementById('player_choice');
-const computer = document.getElementById('compuer_choice');
+const computer = document.getElementById('computer_choice');
 const playerScore = document.getElementById('player_score');
-const computerScore = document.getElementById('computser_score');
+const computerScore = document.getElementById('computer_score');
 const rock_div = document.getElementById('rock_choice');
 const scissors_div = document.getElementById('scissors_choice');
 const paper_div = document.getElementById('paper_choice');
@@ -9,16 +9,62 @@ const paper_div = document.getElementById('paper_choice');
 
 let pScore = 0;
 let cScore = 0; 
-function computer_choice() {
+
+
+
+function randomChoice() {
     const choice = ["r", "p", "s"];
-    let computerChoice = choice[Math.floor(Math.random() * 3)];
-    return computerChoice;
+    let randomChoice = choice[Math.floor(Math.random() * 3)];
+    switch(randomChoice) {
+        case "r":
+        computer.innerHTML = "✊"
+        break;
+        case "s":
+        computer.innerHTML = "✌️"
+        break; 
+        case "p":
+        computer.innerHTML = "✋"
+        break;   
+    }
+    return randomChoice;
 }
-console.log(computer_choice());
+
+
 function game (userChoice) {
-
+    const computerChoice = randomChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "sp":
+        case "pr":
+            win()
+            break;
+        case "sr":
+        case "ps":
+        case "rp":
+            lose()
+            
+            break;
+        case "ss":
+        case "rr":
+        case "pp":
+            drawn()
+            break;
+    }
 }
 
+function win(){
+    pScore++
+    playerScore.innerHTML = "Player => " + pScore;
+};
+
+function lose(){
+    cScore++
+    computerScore.innerHTML ="Computer => " + cScore;
+};
+
+function drawn(){
+
+}
 
 function main (){
    rock_div.addEventListener('click', function() {
